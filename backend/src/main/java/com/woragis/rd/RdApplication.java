4adf6229cbe0860ee949c95834c7f2e282d4a501
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-30 22:51:49 -0300

    Init spring boot backend

---
+++ backend/src/main/java/com/woragis/rd/RdApplication.java
@@ -0,0 +1,13 @@
+package com.woragis.rd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/woragis/rd/RdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woragis.rd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
